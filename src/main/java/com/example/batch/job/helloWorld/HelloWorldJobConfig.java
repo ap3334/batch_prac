aafbefa8beefe2007feced07435905aaf24749d5
ap3334 <chwil2989@naver.com>
2022-10-13 22:51:18 +0900

    Incrementer 설정으로 매 실행마다 Job 실행

--- src/main/java/com/example/batch/job/helloWorld/HelloWorldJobConfig.java
+++ src/main/java/com/example/batch/job/helloWorld/HelloWorldJobConfig.java
@@ -3,10 +3,9 @@ package com.example.batch.job.helloWorld;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
@@ -24,6 +23,7 @@ public class HelloWorldJobConfig {
     public Job helloWorldJob() {
 
         return jobBuilderFactory.get("helloWorldJob")
+                .incrementer(new RunIdIncrementer())
                 .start(helloWorldStep1())
                 .build();
 
