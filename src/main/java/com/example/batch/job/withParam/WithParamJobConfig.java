b7761a802b3dbed6d3f5c0e6b0d42d190e436787
ap3334 <chwil2989@naver.com>
2022-10-14 14:24:27 +0900

    파라미터 전달, 파라미터를 Tasklet 에서 받기

--- src/main/java/com/example/batch/job/withParam/WithParamJobConfig.java
+++ src/main/java/com/example/batch/job/withParam/WithParamJobConfig.java
@@ -1,6 +1,7 @@
 package com.example.batch.job.withParam;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
@@ -9,9 +10,11 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class WithParamJobConfig {
@@ -21,31 +24,34 @@ public class WithParamJobConfig {
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job withParamJob() {
+    public Job withParamJob(Step withParamStep1) {
 
         return jobBuilderFactory.get("withParamJob")
-                .start(withParamStep1())
+                .start(withParamStep1)
                 .build();
 
     }
 
     @Bean
     @JobScope
-    public Step withParamStep1() {
+    public Step withParamStep1(Tasklet withParamStep1Tasklet) {
 
         return stepBuilderFactory.get("withParamStep1")
-                .tasklet(withParamStep1Tasklet())
+                .tasklet(withParamStep1Tasklet)
                 .build();
 
     }
 
     @Bean
     @StepScope
-    public Tasklet withParamStep1Tasklet() {
+    public Tasklet withParamStep1Tasklet(
+            @Value("#{jobParameters['name']}") String name,
+            @Value("#{jobParameters['age']}") String age
+    ) {
 
         return (contribution, chunkContext) -> {
 
-            System.out.println("With Param Tasklet1!!");
+            log.info("name : {}, age : {}", name, age);
 
             return RepeatStatus.FINISHED;
 
