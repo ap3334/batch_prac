64de3a3bebd9c540a6515eb6b547f1d4d8fdbba3
ap3334 <chwil2989@naver.com>
2022-10-14 14:02:53 +0900

    With Param Job 추가

---
+++ src/main/java/com/example/batch/job/withParam/WithParamJobConfig.java
@@ -0,0 +1,55 @@
+package com.example.batch.job.withParam;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class WithParamJobConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job withParamJob() {
+
+        return jobBuilderFactory.get("withParamJob")
+                .start(withParamStep1())
+                .build();
+
+    }
+
+    @Bean
+    @JobScope
+    public Step withParamStep1() {
+
+        return stepBuilderFactory.get("withParamStep1")
+                .tasklet(withParamStep1Tasklet())
+                .build();
+
+    }
+
+    @Bean
+    @StepScope
+    public Tasklet withParamStep1Tasklet() {
+
+        return (contribution, chunkContext) -> {
+
+            System.out.println("With Param Tasklet1!!");
+
+            return RepeatStatus.FINISHED;
+
+        };
+    }
+
+}
