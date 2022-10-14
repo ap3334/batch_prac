86fe6b7ca3aba12ddbb79a5edc80cb164a99202b
ap3334 <chwil2989@naver.com>
2022-10-14 20:57:57 +0900

    상품 옵션 추가

--- src/main/java/com/example/batch/app/product/service/ProductService.java
+++ src/main/java/com/example/batch/app/product/service/ProductService.java
@@ -1,9 +1,12 @@
 package com.example.batch.app.product.service;
 
 import com.example.batch.app.product.entity.Product;
+import com.example.batch.app.product.entity.ProductOption;
 import com.example.batch.app.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -11,13 +14,19 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-    public void create(String name, int price, String makerShopName) {
+    public void create(String name, int price, String makerShopName, List<ProductOption> options) {
 
         Product product = Product.builder()
                 .name(name)
                 .price(price)
                 .makerShopName(makerShopName)
                 .build();
+
+        productRepository.save(product);
+
+        for ( ProductOption option : options ) {
+            product.addOption(option);
+        }
 
         productRepository.save(product);
 
