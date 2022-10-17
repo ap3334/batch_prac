2a91e62f76ad8c5b9330fa1866bdc94ce9efcdba
ap3334 <chwil2989@naver.com>
2022-10-17 17:04:14 +0900

    주문품목정산 잡 구현

---
+++ src/main/java/com/example/batch/app/order/entity/RebateOrderItem.java
@@ -0,0 +1,85 @@
+package com.example.batch.app.order.entity;
+
+import com.example.batch.app.base.entity.BaseEntity;
+import com.example.batch.app.product.entity.ProductOption;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@SuperBuilder
+@ToString(callSuper = true)
+public class RebateOrderItem extends BaseEntity {
+    @OneToOne(fetch = LAZY)
+    @ToString.Exclude
+    private OrderItem orderItem;
+
+    @ManyToOne(fetch = LAZY)
+    @ToString.Exclude
+    private Order order;
+
+    @ManyToOne(fetch = LAZY)
+    private ProductOption productOption;
+
+    private int quantity;
+
+    private int price;
+    private int salePrice;
+    private int wholesalePrice;
+    private int pgFee;
+    private int payPrice;
+    private int refundPrice;
+    private int refundQuantity;
+    private boolean isPaid;
+
+    public RebateOrderItem(OrderItem orderItem) {
+        this.orderItem = orderItem;
+        order = orderItem.getOrder();
+        productOption = orderItem.getProductOption();
+        quantity = orderItem.getQuantity();
+        price = orderItem.getPrice();
+        salePrice = orderItem.getSalePrice();
+        wholesalePrice = orderItem.getWholesalePrice();
+        pgFee = orderItem.getPgFee();
+        payPrice = orderItem.getPrice();
+        refundPrice = orderItem.getRefundPrice();
+        refundQuantity = orderItem.getRefundQuantity();
+        isPaid = orderItem.isPaid();
+    }
+
+    public RebateOrderItem(ProductOption productOption, int quantity) {
+        this.productOption = productOption;
+        this.quantity = quantity;
+        this.price = productOption.getPrice();
+        this.salePrice = productOption.getSalePrice();
+        this.wholesalePrice = productOption.getWholesalePrice();
+    }
+
+    public int calculatePayPrice() {
+        return salePrice * quantity;
+    }
+
+    public void setPaymentDone() {
+        this.pgFee = 0;
+        this.payPrice = calculatePayPrice();
+        this.isPaid = true;
+    }
+
+    public void setRefundDone() {
+        if (refundQuantity == quantity) return;
+
+        this.refundQuantity = quantity;
+        this.refundPrice = payPrice;
+    }
+}
