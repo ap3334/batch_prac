25fcd501f1cea32fa32c67972778f38c47588148
ap3334 <chwil2989@naver.com>
2022-10-17 15:42:35 +0900

    2번 회원 주문 예치금 결제

--- src/main/java/com/example/batch/app/base/DevInitData.java
+++ src/main/java/com/example/batch/app/base/DevInitData.java
@@ -41,18 +41,33 @@ public class DevInitData {
             Product product1 = productService.create("단가라 OPS", 68000, 45000, "청평화 A-1-15", Arrays.asList(new ProductOption("RED", "44"), new ProductOption("RED", "55"), new ProductOption("BLUE", "44"), new ProductOption("BLUE", "55")));
             Product product2 = productService.create("쉬폰 OPS", 72000, 55000, "청평화 A-1-15", Arrays.asList(new ProductOption("WHITE", "44"), new ProductOption("WHITE", "55"), new ProductOption("BLACK", "44"), new ProductOption("BLACK", "55")));
 
-            ProductOption productOption__RED_44 = product1.getProductOptions().get(0);
-            ProductOption productOption__BLUE_44 = product1.getProductOptions().get(2);
+            ProductOption product1Option__RED_44 = product1.getProductOptions().get(0);
+            ProductOption product1Option__BLUE_44 = product1.getProductOptions().get(2);
 
-            cartService.addItem(member1, productOption__RED_44, 1);
-            cartService.addItem(member1, productOption__RED_44, 2);
-            cartService.addItem(member1, productOption__BLUE_44, 1);
+            cartService.addItem(member1, product1Option__RED_44, 1);
+            cartService.addItem(member1, product1Option__RED_44, 2);
+            cartService.addItem(member1, product1Option__BLUE_44, 1);
 
             Order order1 = orderService.createFromCart(member1);
 
             int order1PayPrice = order1.calculatePayPrice();
 
             orderService.payByRestCashOnly(order1);
+
+            ProductOption product2Option__BLACK_44 = product1.getProductOptions().get(0);
+            ProductOption product2Option__WHITE_44 = product1.getProductOptions().get(2);
+
+            cartService.addItem(member2, product1Option__RED_44, 1);
+            cartService.addItem(member2, product2Option__BLACK_44, 2);
+            cartService.addItem(member2, product2Option__WHITE_44, 2);
+
+            Order order2 = orderService.createFromCart(member2);
+
+            memberService.addCash(member2, 370_000, "충전__무통장입금");
+
+            orderService.payByRestCashOnly(order2);
+
+
         };
     }
 
